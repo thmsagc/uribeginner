12ddeab8fc895cbd995a620d9b1fc890f0da681f
thmsagc <52658569+thmsagc@users.noreply.github.com>
2020-05-07 08:10:49 -0300

    Add files via upload

---
+++ 1016.cpp
@@ -0,0 +1,21 @@
+/*
+	@author Thomás Augusto Gouveia Chaves
+	@version 07/05/2020 (d/m/y)
+*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+ 
+int main() {
+
+    int distance, time;
+
+    cin >> distance;
+
+    time = distance*2;
+
+    cout << time << " minutos\n";
+    return 0;
+}
